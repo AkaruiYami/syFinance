f19d7671ec85747989906de16a4ac619d28e3b07
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2025-11-09 18:50:47 +0800

    add new_page command

--- manage.py
+++ manage.py
@@ -5,11 +5,38 @@ from utils.db import init_db
 
 
 # ---------------------------------------------
+#   CUSTOM FUNCTIONS
+# ---------------------------------------------
+def create_new_page():
+    if len(sys.argv) < 3:
+        print("Missing page_name aqgument.")
+        print("Example usage:")
+        print("`python manage.py new_page my_new_page_name`")
+        return
+    page_name = sys.argv[2]
+    page_file = f"./pages/p_{page_name}.py"
+    with open(page_file, "w") as f:
+        title = page_name.replace("_", " ").title()
+        template_str = f"""import streamlit as st
+
+st.set_page_config(page_title="{title}", layout="wide")
+
+st.title("{title}")
+st.markdown("A simple {title} page.")
+st.divider()
+        """
+        f.write(template_str)
+
+    print(f"New file created -> [{page_file}]")
+
+
+# ---------------------------------------------
 # COMMAND REGISTRY
 # ---------------------------------------------
 # Map command names to functions
 COMMANDS = {
     "init_db": init_db,
+    "new_page": create_new_page,
 }
 
 
