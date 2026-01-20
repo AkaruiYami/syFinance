f725bd1374d1481a5f1e14d6f1d179bb77358066
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2026-01-20 15:33:49 +0800

    add form to add user

--- admin/api.py
+++ admin/api.py
@@ -1,6 +1,9 @@
+from models.user import User
+from argon2 import PasswordHasher
+
+
 def get_users(limit=50):
     assert limit > 0, "Limit must be 1 or more."
-    from models.user import User
 
     if limit == 1:
         user = User.objects().first()
@@ -9,3 +12,14 @@ def get_users(limit=50):
     users = User.objects().all()
     users = users[:limit]
     return users
+
+
+def create_user(name, password, email, description):
+    ph = PasswordHasher()
+
+    new_user_data = {}
+    new_user_data["name"] = name.strip()
+    new_user_data["password"] = ph.hash(password)
+    new_user_data["email"] = email.strip()
+    new_user_data["description"] = description.strip()
+    User.new(**new_user_data)
--- admin/pages/p_home.py
+++ admin/pages/p_home.py
@@ -1,14 +1,9 @@
-from datetime import datetime
-import streamlit as st
-import pandas as pd
 import altair as alt
+import pandas as pd
+import streamlit as st
 
 from admin import api
 from utils import config
-from models.income import Income
-from models.transaction import Transactions
-
-
 from utils.auth import require_login
 
 require_login()
@@ -19,16 +14,21 @@ st.title(config.APP_NAME)
 st.markdown("Under construction")
 st.divider()
 
+users = api.get_users()
+n_user = len(users)
+st.metric("Number of User", n_user)
 
 with st.container():
-    st.button("Create User")
+    col1, col2, col3 = st.columns([0.1, 0.1, 0.8])
+    with col1:
+        del_button = st.button("Delete User")
+    with col2:
+        create_button = st.button("Create User")
+    with col3:
+        st.space("stretch")
 
 
 with st.container():
-    users = api.get_users()
-    n_user = len(users)
-    st.metric("Number of User", n_user)
-
     data = {}
     data["User ID"] = [user.id for user in users]
     data["Name"] = [user.name for user in users]
@@ -43,10 +43,39 @@ with st.container():
         hide_index=True,
     )
 
-    selected_ids = edited.index[edited["Select"]].tolist()
+    selected_user_ids = edited.loc[edited["Select"], "User ID"].tolist()
 
-    if selected_ids:
-        st.dataframe(
-            df.loc[selected_ids],
-            hide_index=True,
-        )
+    if selected_user_ids:
+        st.dataframe(df[df["User ID"].isin(selected_user_ids)], hide_index=True)
+
+
+@st.dialog("Create User")
+def create_user_dialog():
+    with st.form("create_user_form", clear_on_submit=True):
+        name = st.text_input("Name", placeholder="e.g. AkaruiYami")
+        password = st.text_input("Password", type="password")
+        email = st.text_input("Email")
+        desc = st.text_input("Descrtiption")
+
+        submitted = st.form_submit_button("Create")
+        if submitted:
+            name = name.strip()
+            if not name:
+                st.error("Name is required.")
+                st.stop()
+
+            password = password.strip()
+            if not password:
+                st.error("Password is required.")
+                st.stop()
+
+            try:
+                api.create_user(name, password, email, desc)
+                st.success(f"User '{name}' created.")
+                st.rerun()  # refresh table + metrics
+            except Exception as e:
+                st.error(f"Failed to create user: {e}")
+
+
+if create_button:
+    create_user_dialog()
